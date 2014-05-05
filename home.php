9c9bcc2e720551a0809ca1852bead90328958b77
chitownsc <chitownsc@gmail.com>
2014-05-05 04:17:20 -0400

    Fixed bug Woooooottt

--- home.php
+++ home.php
@@ -1,27 +1,21 @@
 <? include ("top.php"); ?>
 
-
-
-
 <body id="home">
 
-<!-- <?// include ("header.php"); ?> -->
+	<div id="headerWrapper">
+		<div id="mainTitle">
+			<h1> Lauren Fiasconaro </h1>
+			<h2> Photography </h2>
+		</div><!--close header wrapper-->
+		<div id="navLinks">
+			<ol>
+				<li><a href="bio.php">About Lauren</a></li>
+				<li><a href="terra.php">Terra Mortalis</a></li>
+				<li><a href="gallery.php">Galleries</a></li>
+			</ol>
+		</div><!--close navLinks wrapper-->
+		
+	</div><!--close header wrapper-->
 
-<!-- <? //include ("menu.php"); ?>  -->
-
-<h1> Lauren Fiasconaro </h1>
-<h2> Photography </h2>
-
-<div id="buttonLinks">
-<ol>
-
-	<li><a href="bio.php">About Lauren</a></li>
-	<li><a href="terra.php">Terra Mortalis</a></li>
-	<li><a href="gallery.php">Galleries</a></li>
-
-</ol>
-</div>
-
-<!-- <?// include ("footer.php"); ?> -->
 </body>
 </html>
