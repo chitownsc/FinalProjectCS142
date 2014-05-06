ef6f202b6b69783d82300da968e4bca61a6830a2
TheFyx <jaysoncj@gmail.com>
2014-05-06 11:43:17 -0400

    Uability everything

---
+++ Usability/lf.php
@@ -0,0 +1,60 @@
+body{
+    box-shadow: 20px 20px 50px black;
+    padding-left: 2em;
+    padding-right: 2em;
+    padding-top: 1em;
+    padding-bottom: 2em;
+    width:7.5in;
+    background-color: white;
+    margin-left:auto;
+    margin-right:auto;
+    margin-bottom: 100px;
+    
+}
+
+html{
+    margin: auto;
+    background-color: lightgray;
+}
+
+#heading{
+    border-bottom: solid blue;
+    text-align: center;
+     
+}
+
+#groupNames{
+    text-align: center;
+    font-size: 1em;
+    margin-bottom: 50px;
+    color: black;
+}
+
+#mypaceLink{
+    border: solid thin black;
+    background-color: gray;
+    text-align: center;
+    padding: .5em;
+    margin-left: 175px;
+    
+}
+
+div{
+    margin:auto;
+    margin-left: 75px;
+    padding-bottom: 10em;
+
+
+}
+
+h3{
+    text-align: center;
+    font-size: 24pt;
+}
+#mypaceReview{
+    border: solid thin black;
+    background-color: gray;
+    text-align: center;
+    padding: .5em;
+    
+}
