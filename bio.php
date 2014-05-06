4d6c1920ca6d3bd4ce29f38177bcbe36b54b0a08
chitownsc <chitownsc@gmail.com>
2014-05-06 00:17:04 -0400

    merge style sheet

---
+++ bio.php
@@ -0,0 +1,28 @@
+<? include ("top.php"); ?>
+
+<body id="bio">
+
+	<div id="headerWrapper">
+	<? include ("menu2.php"); ?>
+		<div id="mainTitle">
+			<h1> Lauren Fiasconaro </h1>
+			<h2> About Me</h2>
+		</div><!--close header wrapper-->
+		<div id="articleWrap">
+		<img id= "myFace" src="helloWorld.jpg" alt="Lauren The Artistic Artist">
+			<article id="articleBio">
+				<p> I am an artist working the medium of photography to explore 
+				what happens when post-industrial people come in contact with their 
+				environments. I seek to question human ethics in terms of our ecological 
+				place on the planet, as I feel an impending sense of urgency regarding the 
+				degradation of the global environment. 
+				</p>
+
+
+			</article>
+		</div><!--close navLinks wrapper-->
+		
+	</div><!--close header wrapper-->
+
+</body>
+</html>
