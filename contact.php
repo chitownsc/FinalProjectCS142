30fd0c80f142b8ac93b67dcd97eb68535ad688dc
TheFyx <jaysoncj@gmail.com>
2014-05-06 03:03:44 -0400

    contact page the php the movie of love and life amen jesus loves you this I know  PFMA

---
+++ contact.php
@@ -0,0 +1,23 @@
+<? include ("top.php"); ?>
+
+<body id="contact">
+
+	<div id="headerWrapper">
+	<? include ("menu2.php"); ?>
+		<div id="mainTitle">
+			<h1> Lauren Fiasconaro </h1>
+			<h2> Contact Information </h2>
+		</div><!--close header wrapper-->
+		<div id="conInfo">
+			
+			<h3>Email:</h3>
+			<h3>lfphotag@gmail.com</h3>
+			<h3>Phone:</h3>
+			<h3>1-802-867-5309</h3>
+
+		</div><!--close navLinks wrapper-->
+		
+	</div><!--close header wrapper-->
+<? include ("footer.php"); ?>
+</body>
+</html>
