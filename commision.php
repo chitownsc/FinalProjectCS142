aea3277bcd7773d146bca9dd6c81d08b8fc86a08
chitownsc <chitownsc@gmail.com>
2014-05-06 10:23:58 -0400

    First gallery working minus javascript

--- commision.php
+++ commision.php
@@ -1,8 +1,33 @@
 <? include ("top.php"); ?>
+	</head>
 	<body>
+		<div id="headerWrapper">
+			<? include ("menu2.php"); ?>
+			<div id="mainTitle">
+				<h1> Lauren Fiasconaro </h1>
+				<h2> Commision </h2>
+			</div><!--close title wrapper-->
+
 		<div id="mainWrapper" class="main">
-			<img id="commisionImg" class="main" onclick="displayNextImage('commision', 8)"  onload="autoPlay('commision', 8)" src="images/commision/regular/photo0.png">
+			<div id="theaterContainer">
+				<div id="picContainer">
+					<img id="commisionImg" class="main" onclick="displayNextImage('commision', 8)"  onload="autoPlay('commision', 8)" src="images/commision/regular/photo0.png">
+				</div>
+				<div id="thumbScrollbar">
+					<img onclick="" src="images/commision/thumb/photo0.jpg">
+					<img onclick="" src="images/commision/thumb/photo1.jpg">
+					<img onclick="" src="images/commision/thumb/photo2.jpg">
+					<img onclick="" src="images/commision/thumb/photo3.jpg">
+					<img onclick="" src="images/commision/thumb/photo4.jpg">
+					<img onclick="" src="images/commision/thumb/photo5.jpg">
+					<img onclick="" src="images/commision/thumb/photo6.jpg">
+					<img onclick="" src="images/commision/thumb/photo7.jpg">
+					<img onclick="" src="images/commision/thumb/photo8.jpg">
+					<img onclick="" src="images/commision/thumb/photo9.jpg">
+				</div>
+			</div>
 			
 		</div>
+	</div>
 	</body>
 </html>	
--- gallery.php
+++ gallery.php
@@ -12,21 +12,21 @@
 				<div class="wrapRow">
 					<div class="wrap">
 						<img id="thesisCover" onclick="window.location = 'thesis.php';" class="cover" src="images/cover/photo0.png">
-						<img id="thesisImg" class="coverImg" onload="autoPlayAll()" src="images/thesis/small/photo0.png">
+						<img id="thesisImgCover" class="coverImg" onload="autoPlayAll()" src="images/thesis/small/photo0.png">
 					</div>
 					<div class="wrap">
 						<img id="courseWorkCover" onclick="window.location = 'courseWork.php';" class="cover" src="images/cover/photo1.png">
-						<img id="courseWorkImg" class="coverImg" src="images/courseWork/small/photo0.png">
+						<img id="courseWorkImgCover" class="coverImg" src="images/courseWork/small/photo0.png">
 					</div>
 				</div>
 				<div class="wrapRow">
 					<div class="wrap">
 						<img id="travelCover" onclick="window.location = 'travel.php';" class="cover" src="images/cover/photo2.png">
-						<img id="travelImg" class="coverImg" src="images/travel/small/photo0.png">
+						<img id="travelImgCover" class="coverImg" src="images/travel/small/photo0.png">
 					</div>
 					<div class="wrap">
 						<img id="commisionCover" onclick="window.location = 'commision.php';" class="cover" src="images/cover/photo3.png">
-						<img id="commisionImg" class="coverImg" src="images/commision/small/photo0.png">
+						<img id="commisionImgCover" class="coverImg" src="images/commision/small/photo0.png">
 					</div>
 				</div>
 			</div>
