7ce560fd242f2f43002ea79846261e95f72dc0bb
chitownsc <chitownsc@gmail.com>
2014-05-06 02:02:21 -0400

    Updated Gallery

--- gallery.php
+++ gallery.php
@@ -2,15 +2,25 @@
 
 	</head>
 	<body>
+	
+
 		<div id="mainWrapper" class="main">
-			<img id="thesisImg" onload="autoPlayAll()" src="images/thesis/small/photo0.png">
-			<img id="thesisCover" class="cover" src="images/cover/photo0.png">
-			<img id="courseWorkImg" src="images/courseWork/small/photo0.png">
-			<img id="courseWorkCover" class="cover" src="images/cover/photo1.png">
-			<img id="travelImg" src="images/travel/small/photo0.png">
-			<img id="travelCover" class="cover" src="images/cover/photo2.png">
-			<img id="commisionImg" src="images/commision/small/photo0.png">
-			<img id="commisionCover" class="cover" src="images/cover/photo3.png">
+			<div class="wrap">
+				<img id="thesisCover" onclick="window.location = 'thesis.php';" class="cover" src="images/cover/photo0.png">
+				<img id="thesisImg" class="coverImg" onload="autoPlayAll()" src="images/thesis/small/photo0.png">
+			</div>
+			<div class="wrap">
+				<img id="courseWorkCover" onclick="window.location = 'courseWork.php';" class="cover" src="images/cover/photo1.png">
+				<img id="courseWorkImg" class="coverImg" src="images/courseWork/small/photo0.png">
+			</div>
+			<div class="wrap">
+				<img id="travelCover" onclick="window.location = 'travel.php';" class="cover" src="images/cover/photo2.png">
+				<img id="travelImg" class="coverImg" src="images/travel/small/photo0.png">
+			</div>
+			<div class="wrap">
+				<img id="commisionCover" onclick="window.location = 'commision.php';" class="cover" src="images/cover/photo3.png">
+				<img id="commisionImg" class="coverImg" src="images/commision/small/photo0.png">
+			</div>
 		</div>
 	</body>
 </html>	
