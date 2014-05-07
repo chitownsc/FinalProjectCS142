54f4d8034a288f41af6e7e08b127546623eddcf0
TheFyx <jaysoncj@gmail.com>
2014-05-06 21:50:31 -0400

    Links fixed

--- Usability/usability.php
+++ Usability/usability.php
@@ -8,11 +8,11 @@
         <link href='lf.css' rel='stylesheet' type='text/css' media='screen' title='CSS' />
     </head>
     <body>
-        <h1 id ="heading">Web Usability Tests For Myspace</h1>
+        <h1 id ="heading">Web Usability Tests For Lauren Fiasconaro's Photography</h1>
         <h2 id ="groupNames">Jayson Conyette, Sarah Clarke</h2>
         
         <a id ="mypaceLink" href="http://www.uvm.edu/~jconyett/cs142/assignment7/home.php" target="_blank">Lauren Photography Main Website</a>
-        <a id ="mypaceReview" href="   " target="_blank">Lauren Fiasconaro Site Usability</a>
+        <a id ="mypaceReview" href="usabilityTest1.pdf" target="_blank">Lauren Fiasconaro Site Usability</a>
 		
 
         <div>
