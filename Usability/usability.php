c1feb9a9917d8a679623a8d5456940e8c69cc25b
TheFyx <jaysoncj@gmail.com>
2014-05-06 21:20:34 -0400

    The Forms Finalized

--- Usability/usability.php
+++ Usability/usability.php
@@ -13,13 +13,26 @@
         
         <a id ="mypaceLink" href="http://www.uvm.edu/~jconyett/cs142/assignment7/home.php" target="_blank">Lauren Photography Main Website</a>
         <a id ="mypaceReview" href="   " target="_blank">Lauren Fiasconaro Site Usability</a>
-
+		
 
         <div>
         <h3> Usability Tests </h3>
         
-
-
+		<!-- EVAN   -->
+		<iframe width="560" height="315" src="//www.youtube.com/embed/33AP5qM-D6g" frameborder="0" allowfullscreen></iframe>
+		
+		<!-- ED  -->
+		<iframe width="560" height="315" src="//www.youtube.com/embed/syYbxL4RpVQ" frameborder="0" allowfullscreen></iframe>
+	
+		<!-- JAAKA -->
+		
+		<iframe width="560" height="315" src="//www.youtube.com/embed/FvWuySL8Irk" frameborder="0" allowfullscreen></iframe>
+	
+	
+	
+		<!-- TIM -->
+		
+		<iframe width="560" height="315" src="//www.youtube.com/embed/Hj5RlExn998" frameborder="0" allowfullscreen></iframe>
 
 
         </div>
